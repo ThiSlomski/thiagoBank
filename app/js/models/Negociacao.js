8b250b583fb017077dad39776cf4a2915b3d14dc
Thiago Slomski <thiagoslomski@hotmail.com>
2020-12-21 17:51:00 -0300

    Check in ( 2 )

--- app/js/models/Negociacao.js
+++ app/js/models/Negociacao.js
@@ -2,6 +2,18 @@ class Negociacao {
 
     constructor(data,quantidade,valor) {
 
+        if(!data){
+            throw new Error('data deve ser preenchida');
+        };
+
+        if(!quantidade){
+            throw new Error('quantidade deve ser preenchida');
+        };
+
+        if(!valor){
+            throw new Error('valor deve ser preenchido');
+        };
+
         this._data = data;
         this._quantidade = quantidade;
         this._valor = valor;
